4f8dc7f73b82bc6670b8efddfda538246a4fdd40
Obydennov Dmitrii <dima-obydennov@mail.ru>
2023-08-24 16:11:03 +0800

    add Parameters and Option

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,29 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
+class App implements Callable<Integer> {
+    @CommandLine.Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    private String filepath1;
+
+    @CommandLine.Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    private String filepath2;
+
+    @CommandLine.Option(names = {"-f", "--format"}, description = "output format [default: stylish]", paramLabel = "format")
+    private String format = "stylish";
+
+    @Override
+    public Integer call() throws Exception {
+        // ваша логика обработки файлов
+        return 0;
+    }
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
