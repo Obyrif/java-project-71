0df825d79c5b7c57b78c980bd5f0bb98d2de00a5
Obydennov Dmitrii <dima-obydennov@mail.ru>
2023-08-28 17:02:12 +0800

    add

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,12 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
@@ -19,7 +23,15 @@ class App implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        // логика обработки файлов
+        String fileContent1 = Files.readString(Path.of(filepath1));
+        String fileContent2 = Files.readString(Path.of(filepath2));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> json1 = objectMapper.readValue(fileContent1, Map.class);
+        Map<String, Object> json2 = objectMapper.readValue(fileContent2, Map.class);
+
+        String diff = Differ.generate(json1, json2);
+        System.out.println(diff);
         return 0;
     }
     public static void main(String... args) {
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,4 +1,35 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Differ {
+    public static String generate(Map<String, Object> json1, Map<String, Object> json2) throws Exception {
+        Set<String> diffLine = new TreeSet<>();
+
+        for (String key : json1.keySet()) {
+            if(!json2.containsKey(key)) {
+                diffLine.add("- " + key + ": " + json1.get(key));
+            } else {
+                if(!json1.get(key).equals(json2.get(key))) {
+                    diffLine.add("- " + key + ": " + json1.get(key));
+                    diffLine.add("+ " + key + ": " + json2.get(key));
+                }
+            }
+        }
+
+        for (String key: json2.keySet()) {
+            if(!json1.containsKey(key)) {
+                diffLine.add("+ " + key + ": " + json2.get(key));
+            }
+        }
+
+        return String.join("\n", diffLine);
+    }
 }
