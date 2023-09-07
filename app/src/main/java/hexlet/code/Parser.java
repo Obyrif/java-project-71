763e8bd842eb2820ae077a9534b3d8b8a5259221
Obydennov Dmitrii <dima-obydennov@mail.ru>
2023-09-07 23:03:56 +0800

    add NewTest

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,9 +1,5 @@
 package hexlet.code;
 
-import java.util.Map;
-
 public class Parser {
-    public static String generateYaml(Map<String, Object> json1, Map<String, Object> json2) {
-        return null;
-    }
+
 }
